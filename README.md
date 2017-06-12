c023b6ae9a182765319bca0ac6f2155d318be109
goubin510 <goubinou@hotmail.fr>
2017-06-12 13:54:31 +0200

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# aumgmentFront
+
+Bon chance !
---
+++ cfg.sh
@@ -0,0 +1,7 @@
+echo coucou toi :D;
+open ./ -a Sublime\ Text/;
+open ~/Private/inDev/designIntoDev/DesignIntoDev.github.io -a Sublime\ Text/;
+open http://0.0.0.0:3000/;
+
+echo A plus dans le bus;
+watch-http-server -p 3000;
